d90466d3ade71b87691bc9c46bc86eb10039bcd6
jakebennett1112 <jakebennett1112@gmail.com>
2025-03-29 14:09:41 +1100

    extend with additional arithmetic operations of exponentiation, square root and modulo

--- index.js
+++ index.js
@@ -109,6 +109,79 @@ app.get("/div", (req,res)=>{
       }
 });
 
+// ADVANCED OPERATIONS FOR 4.2C
+// Exponential ie. to the power of
+const exp= (n1,n2) => {
+    return n1**n2;
+}
+app.get("/exp", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    const result = exp(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+// Square root
+const sqrt= (number) => {
+    return Math.sqrt(number);
+}
+app.get("/sqrt", (req,res)=>{
+    try{
+    const number= parseFloat(req.query.number);
+    if(isNaN(number)) {
+        throw new Error("number incorrectly defined");
+    }
+    const result = sqrt(number);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+// Modulo
+const mod= (n1,n2) => {
+    return n1%n2;
+}
+app.get("/mod", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    const result = mod(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
 const port=3040;
 app.listen(port,()=> {
     console.log("hello i'm listening to port " +port);
